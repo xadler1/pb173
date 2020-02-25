34aac5b209a34a6a5718ef5d3f48b7d99fef2c55
xadler1 <469190@mail.muni.cz>
2020-02-25 03:33:11 +0100

    basic functionality done

--- assignment.cpp
+++ assignment.cpp
@@ -1,31 +1,249 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <dlfcn.h>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include <iomanip>
+#include <memory>
 #include "types.hpp"
+
+int encrypt(char *keyarg, char *vecarg, const std::string &inputPath, const std::string &outputPath, void *lib)
+{
+	/* encrypt */
+	mbedtls_aes_context aes;
+	unsigned char key[16];
+	unsigned char iv[16];
+
+	for (size_t i = 0; i < 16; ++i) {
+		key[i] = keyarg[i];
+		iv[i] = vecarg[i];
+	}
+
+	std::vector<unsigned char> input;
+	size_t input_len = 0;
+
+	// library loading
+	typedef int (*aes_setkey_enc_t)(mbedtls_aes_context *, const unsigned char *, unsigned int);
+	typedef int (*aes_crypt_cbc_t)(mbedtls_aes_context *, int, size_t, unsigned char[], const unsigned char *, unsigned char *);
+
+	aes_setkey_enc_t aes_setkey_enc = (aes_setkey_enc_t) dlsym(lib, "mbedtls_aes_setkey_enc");
+	if (!aes_setkey_enc) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+	aes_crypt_cbc_t aes_crypt_cbc = (aes_crypt_cbc_t) dlsym(lib, "mbedtls_aes_crypt_cbc");
+	if (!aes_crypt_cbc) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+	std::fstream fin(inputPath, std::ios::in);
+	std::fstream fout(outputPath, std::ios::out);
+
+	while (!fin.eof()) {
+		++input_len;
+		input.push_back(fin.get());
+	}
+
+	// remove eof character
+	--input_len;
+	input.pop_back();
+
+	// padding
+	size_t pad = input_len % 16;
+	//std::cout << "input_len = " << input_len << std::endl;
+	//std::cout << "pad = " << pad << std::endl;
+
+	for (size_t i = pad; i < 16; ++i) {
+		++input_len;
+		input.push_back(16 - pad);
+
+	}
+
+	unsigned char* in = &input[0];
+	unsigned char output[input_len];
+
+
+	aes_setkey_enc(&aes, key, 128);
+	aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, input_len, iv, in, output);
+
+	fout.write((char *)&output[0], input_len);
+
+	/* hashing */
+	typedef int (*sha512_t)(const unsigned char *, size_t, unsigned char[], int);
+	sha512_t sha512 = (sha512_t) dlsym(lib, "mbedtls_sha512_ret");
+	if (!sha512) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+	std::fstream fouthash(outputPath + ".digest", std::ios::out);
+
+	unsigned char hash[64];
+	const unsigned char *hash_in = (const unsigned char *) output;
+
+	sha512(hash_in, input_len, hash, 0);
+
+	fouthash << std::hex;
+
+	for (size_t i = 0; i < 64; ++i) {
+		fouthash << std::setfill('0') << std::setw(2) << static_cast<unsigned>(hash[i]);
+	}
+
+	fouthash << std::dec << std::endl;
+	return 0;
+}
+
+int decrypt(char *keyarg, char *vecarg, const std::string &inputPath, const std::string &outputPath, void *lib)
+{
+	/* hashing */
+	typedef int (*sha512_t)(const unsigned char *, size_t, unsigned char[], int);
+	sha512_t sha512 = (sha512_t) dlsym(lib, "mbedtls_sha512_ret");
+	if (!sha512) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+	std::vector<unsigned char> input;
+	size_t input_len = 0;
+
+	std::fstream fin(inputPath, std::ios::in);
+	std::fstream fout(outputPath, std::ios::out);
+
+	while (!fin.eof()) {
+		++input_len;
+		input.push_back(fin.get());
+	}
+
+	// last iteration did not add any characters
+	--input_len;
+
+	unsigned char* in = &input[0];
+
+
+	/* hash comparison */
+	unsigned char hash[64];
+	const unsigned char *hash_in = (const unsigned char *) in;
+
+	sha512(hash_in, input_len, hash, 0);
+
+	std::stringstream computed;
+	std::stringstream supplied;
+
+	computed << std::hex;
+
+	for (size_t i = 0; i < 64; ++i) {
+		computed << std::setfill('0') << std::setw(2) << static_cast<unsigned>(hash[i]);
+	}
+
+	computed << std::dec << std::endl;
+
+	std::fstream finhash(inputPath + ".digest", std::ios::in);
+
+	while (!finhash.eof()) {
+		supplied << static_cast<char>(finhash.get());
+	}
+
+	// remove eof character
+	std::string sup = supplied.str();
+	sup.pop_back();
+
+
+	if (computed.str() != sup) {
+		std::cerr << "digests don't match" << std::endl;
+	}
+
+	//std::cout << "computed = " << computed.str() << std::endl;
+	//std::cout << "supplied = " << sup << std::endl;
+	//std::cout << "computed size = " << computed.str().length() << std::endl;
+	//std::cout << "supplied size = " << sup.length() << std::endl;
+
+	//std::cout << input_len << std::endl;
+
+	/* decrypting */
+	mbedtls_aes_context aes;
+	unsigned char key[16];
+	unsigned char iv[16];
+
+	for (size_t i = 0; i < 16; ++i) {
+		key[i] = keyarg[i];
+		iv[i] = vecarg[i];
+	}
+
+	typedef int (*aes_setkey_dec_t)(mbedtls_aes_context *, const unsigned char *, unsigned int);
+	typedef int (*aes_crypt_cbc_t)(mbedtls_aes_context *, int, size_t, unsigned char[], const unsigned char *, unsigned char *);
+
+	aes_setkey_dec_t aes_setkey_dec = (aes_setkey_dec_t) dlsym(lib, "mbedtls_aes_setkey_dec");
+	if (!aes_setkey_dec) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+	aes_crypt_cbc_t aes_crypt_cbc = (aes_crypt_cbc_t) dlsym(lib, "mbedtls_aes_crypt_cbc");
+	if (!aes_crypt_cbc) {
+		std::cerr << dlerror() << std::endl;
+		return 2;
+	}
+
+
+
+	unsigned char output[input_len];
+
+	//std::cout << "input_len = " << input_len << std::endl;
+
+	aes_setkey_dec(&aes, key, 128);
+	aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, input_len, iv, in, output);
+
+	// remove padding
+	size_t pad = output[input_len - 1];
+
+	for (size_t i = 0; i < input_len - pad; ++i) {
+		fout << output[i];
+	}
+
+	//printHash(output, input_len);
+
+
+	return 0;
+}
+
+void printHelp()
+{
+	std::cout << "usage: assignment -en <key> <vector> <input file> <output file>" << std::endl;
+	std::cout << "encrypts <input file> into <output file> and writes sha512 checksum into <output file>.digest" << std::endl;
+	std::cout << std::endl;
+	std::cout << "       assignment -de <key> <vector> <input file> <output file>" << std::endl;
+	std::cout << "decrypts <input file> into <output file> and compare checksum with <input file>.digest" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	if (argc != 6) {
+		printHelp();
+		return 1;
+	}
 
 	// test wether library is present
-	//void *lib = dlopen("./libmbedtls.so", RTLD_LAZY);
-	void *math = dlopen("/lib/libm.so.6", RTLD_LAZY);
+	void *lib = dlopen("./libmbedcrypto.so", RTLD_LAZY);
 
-	mbedtls_sha512_context ctx;
-	auto *pctx = &ctx;
+	if (!lib) {
+		std::cerr << dlerror();
+		return 2;
+	}
 
-	//void (*sha512_init)(mbedtls_sha512_context*) = dlsym(lib, "mbedtls_sha512_init");
-	//
-	typedef double (*kosinus_t)(double);
-	kosinus_t kos = (kosinus_t) dlsym(math, "cos");
+	if (strcmp(argv[1], "-en") == 0) {
+		encrypt(argv[2], argv[3], argv[4], argv[5], lib);
+	} else if (strcmp(argv[1], "-de") == 0) {
+		decrypt(argv[2], argv[3], argv[4], argv[5], lib);
+	} else {
+		printHelp();
+		return 1;
+	}
 
-	std::cout << kos(0.0) << std::endl;
-	std::cout << "NECO" << std::endl;
-
-
-
-	//dlclose(math);
-	//std::cout << "NECO" << std::endl;
-	//dlclose(lib);
-	std::cout << "NECO" << std::endl;
+	dlclose(lib);
 	return 0;
 }
+
--- types.hpp
+++ types.hpp
@@ -1,5 +1,8 @@
 #include <cstdint>
 
+#define MBEDTLS_AES_ENCRYPT     1 /**< AES encryption. */
+#define MBEDTLS_AES_DECRYPT     0 /**< AES decryption. */
+
 typedef struct mbedtls_sha512_context
 {
     uint64_t total[2];          /*!< The number of Bytes processed. */
@@ -9,3 +12,18 @@ typedef struct mbedtls_sha512_context
                                      0: Use SHA-512, or 1: Use SHA-384. */
 }
 mbedtls_sha512_context;
+
+typedef struct mbedtls_aes_context
+{
+    int nr;                     /*!< The number of rounds. */
+    uint32_t *rk;               /*!< AES round keys. */
+    uint32_t buf[68];           /*!< Unaligned data buffer. This buffer can
+                                     hold 32 extra Bytes, which can be used for
+                                     one of the following purposes:
+                                     <ul><li>Alignment if VIA padlock is
+                                             used.</li>
+                                     <li>Simplifying key expansion in the 256-bit
+                                         case by generating an extra round key.
+                                         </li></ul> */
+}
+mbedtls_aes_context;
